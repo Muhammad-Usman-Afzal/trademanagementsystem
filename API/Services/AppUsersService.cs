620c140ed1c79c7f3c6bd9d3b14e523ba996dfee
SharjeelSultan <sharjeel.designer@gmail.com>
2025-03-01 05:24:10 +0500

    jwt done

--- API/Services/AppUsersService.cs
+++ API/Services/AppUsersService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Composition;
+using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -17,7 +20,7 @@ public class AppUsersService : BaseService<AppUsers>, IAppUsersRepo
         _configuration = configuration;
     }
 
-    public string GenerateToken(AppUsers oUser)
+    public string exGenerateToken(AppUsers oUser)
     {
         try
         {
@@ -49,6 +52,67 @@ public class AppUsersService : BaseService<AppUsers>, IAppUsersRepo
             string TokenString = "";
             TokenString = new JwtSecurityTokenHandler().WriteToken(Token);
             return TokenString;
+        }
+        catch (Exception ex)
+        {
+            APILogger.WriteLog(ex);
+            return "";
+        }
+    }
+    
+    public string xGenerateToken(AppUsers oUser)
+    {
+        try
+        {
+            string role = "Guest";
+            var claims = new List<Claim>
+            {
+                                        new Claim(JwtRegisteredClaimNames.Sub,oUser.Id.ToString()),
+                                        new Claim(JwtRegisteredClaimNames.Name,oUser.UserName),
+                                        new Claim("Role",role),
+                                        };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("TechnologyiansSolutionsKiSecretKeyBuhatHiSecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var Sectoken = new JwtSecurityToken(
+                null,
+                null,
+            claims,
+            expires: DateTime.Now.AddMinutes(double.Parse("60")), // will be valid one hour
+                signingCredentials: credentials);
+            var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+            return token;
+        }
+        catch (Exception ex)
+        {
+            APILogger.WriteLog(ex);
+            return "";
+        }
+    }
+
+    private string GenerateToken(AppUsers oUser)
+    {
+        try
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var secretKey = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, oUser.UserName),
+                new Claim(ClaimTypes.Role, "Admin")
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiryMinutes"])),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
         catch (Exception ex)
         {
--- API/namespaces.cs
+++ API/namespaces.cs
@@ -10,3 +10,4 @@ global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.IdentityModel.Tokens;
 global using System.Text;
+global using Microsoft.OpenApi.Models;
